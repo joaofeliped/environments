ce1c2096ad421306d69686f3c321c19feb9c4dca
João Felipe <jo_5919@hotmail.com>
2020-04-04 15:02:57 +0000

    Update install_debian_10.sh

--- install_debian_10.sh
+++ install_debian_10.sh
@@ -48,6 +48,11 @@ if [[ "$EUID" = 0 ]]; then
     software-properties-common
   
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
+     
   sudo apt-get update
 
   sudo apt-get install docker-ce docker-ce-cli containerd.io
